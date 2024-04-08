cfe146cbca1e174daf89c3e47fb5e1a59abf4961
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-04-08 13:56:05 +0530

    added the routes in hono

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,10 +2,6 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
 app.post('/api/v1/user/signup', (c) => {
   // this route is for user signup
 })
@@ -18,16 +14,18 @@ app.post('/api/v1/blog', (c) => {
   // this route is for posting a new blog
 })
 
-app.put('/api/v1/blog', (C) => {
+app.put('/api/v1/blog', (c) => {
   // this route is for updating any blog
 })
 
 app.get('/api/v1/blog/:id', (c) => {
   // this route is for getting back a blog with a specific id
+  return c.text("blogs with a particular id")
 })
 
 app.get('/api/v1/blog/bulk', (c) => {
   // this route is for getting back all the routes
+  return c.text("blogs")
 })
 
 export default app
