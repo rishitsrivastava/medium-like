b085158a2720148eb04c5ca76ab9554310f260fa
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-04-08 13:52:06 +0530

    initialised hono

---
+++ backend/src/index.ts
@@ -0,0 +1,33 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/user/signup', (c) => {
+  // this route is for user signup
+})
+
+app.post('/api/v1/user/signin', (c) => {
+  // this route is for user signin
+})
+
+app.post('/api/v1/blog', (c) => {
+  // this route is for posting a new blog
+})
+
+app.put('/api/v1/blog', (C) => {
+  // this route is for updating any blog
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+  // this route is for getting back a blog with a specific id
+})
+
+app.get('/api/v1/blog/bulk', (c) => {
+  // this route is for getting back all the routes
+})
+
+export default app
