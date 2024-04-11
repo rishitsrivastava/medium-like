7f589364d1eb28a63c2adbd2ebbf1addfedf08a1
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-04-11 16:52:34 +0530

    created User and Posts model in prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
 const app = new Hono()
 
 app.post('/api/v1/user/signup', (c) => {
   // this route is for user signup
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL, 
+  }).$extends(withAccelerate())
   return c.text('hello');
 })
 
@@ -11,6 +16,7 @@ app.post('/api/v1/user/signin', (c) => {
   // this route is for user signin
   return c.text('hello');
 })
+9873816182
 
 app.post('/api/v1/blog', (c) => {
   // this route is for posting a new blog
