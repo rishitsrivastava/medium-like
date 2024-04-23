96975b13a6067e84f51476380f36410b28f8d4a9
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-04-24 01:12:15 +0530

    published on npm and used it on backend routes

--- backend/routes/blog.ts
+++ backend/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
+import { createBlogInput, updateBlogInput } from "@rishit1618/common";
 
 export const blogRouter = new Hono<{
     Bindings: {
@@ -33,6 +34,14 @@ blogRouter.use('/*', async (c, next) => {
 
 blogRouter.post('/', async (c) => {
     const body = await c.req.json();
+    const {success} = createBlogInput.safeParse(body)
+    if(!success) {
+      c.status(411)
+      return c.json({
+        message: "Inputs are wrong"
+      })
+    }
+    
     const authorId = c.get("userId")
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL
@@ -53,6 +62,14 @@ blogRouter.post('/', async (c) => {
   
 blogRouter.put('/', async (c) => {
   const body = await c.req.json();
+  const {success} = updateBlogInput.safeParse(body)
+  if(!success) {
+    c.status(411)
+    return c.json({
+      message: "Inputs are wrong"
+    })
+  }
+
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL
   }).$extends(withAccelerate());
--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -1,8 +1,9 @@
-import { signupInput } from './../../common/src/index';
+
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
+import { signupInput, signinInput } from '@rishit1618/common';
 
 export const userRouter = new Hono<{
     Bindings: {
@@ -53,6 +54,13 @@ userRouter.post('/signup', async (c) => {
     
     try {
       const body = await c.req.json();
+      const {success} = signupInput.safeParse(body)
+      if(!success) {
+        c.status(411)
+        return c.json({
+          message: "Inputs are wrong"
+        })
+      }
       const user = await prisma.user.findUnique({
         where: {
           email: body.email,
