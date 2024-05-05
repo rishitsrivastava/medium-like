8f42dac323e5d91e34302e8bec30fcbfec1f65cf
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-05-05 16:19:07 +0530

    completed and added appbar component

---
+++ frontend/src/components/Appbar.tsx
@@ -0,0 +1,12 @@
+import { Avatar } from "./BlogCard"
+
+export const Appbar = () => {
+    return <div className="border-b flex justify-between px-10 py-4">
+        <div className="font-bold flex flex-col justify-center">
+            Medium
+        </div>
+        <div>
+            <Avatar name="Rishit" size={7}/>
+        </div>
+    </div>
+}
--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -41,9 +41,9 @@ export const BlogCard = ({
     </div>
 }
 
-function Avatar({name} : {name: string}) {
-    return <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-        <span className="font-xs font-extralight text-gray-600 dark:text-gray-300">{name[0]}</span>
+export function Avatar({name, size = 6} : {name: string, size?: number}) {
+    return <div className={`relative inline-flex items-center justify-center w-${size} h-${size} overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600`}>
+        <span className="text-xs font-extralight text-gray-600 dark:text-gray-300">{name[0]}</span>
     </div>
     
 }
